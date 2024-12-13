18d64a445a93edbd059fa9b46e49511746e20874
yazanaltonji012 <yazanaltonji01@gmail.com>
2024-12-13 16:26:21 +0300

    u

--- public/module/share.js
+++ public/module/share.js
@@ -9,6 +9,7 @@
   products.forEach(p => {
     product.push(p);
   });
+let url = `https://www.facebook.com/sharer/sharer.php?u=https://modern-lab-alep.onrender.com/`;
   let div = document.createElement('div')
   let continer = document.createElement('div')
   let divsmall = document.createElement('div')
@@ -55,11 +56,11 @@
   continer.appendChild(divsmall3)
   div.appendChild(img4)
   div.appendChild(continer)
-  img1.addEventListener("click", () => {
-    window.location.href = "https://www.facebook.com/sharer/sharer.php?u=https://modern-lab-alepp.web.app/"
+  divsmall.addEventListener("click", () => {
+    window.location.href = url;
   })
-  img3.addEventListener("click", () => {
-    window.location.href = "https://api.whatsapp.com/send?text=https://modern-lab-alepp.web.app/l"
+  divsmall3.addEventListener("click", () => {
+    window.location.href = url;
   })
   img4.addEventListener("click", () => {
     setTimeout(() => {
@@ -70,15 +71,19 @@
   })
 
   share[0].addEventListener("click", () => {
+    url = "https://www.facebook.com/sharer/sharer.php?u=https://modern-lab-alep.onrender.com/Analyser%20Sweden"
     product[0].appendChild(div);
   })
   share[1].addEventListener("click", () => {
+    url = "https://www.facebook.com/sharer/sharer.php?u=https://modern-lab-alep.onrender.com/Analyser%20Germany"
     product[1].appendChild(div);
   })
   share[2].addEventListener("click", () => {
+    url = "https://www.facebook.com/sharer/sharer.php?u=https://modern-lab-alep.onrender.com/Analyser%20chinese"
     product[2].appendChild(div);
   })
   share[3].addEventListener("click", () => {
+    url = "https://www.facebook.com/sharer/sharer.php?u=https://modern-lab-alep.onrender.com/ميزان%20دقيق"
     product[3].appendChild(div);
   })
 
